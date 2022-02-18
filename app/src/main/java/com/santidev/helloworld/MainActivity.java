1fb5dd58c1edabe17e55c9fa8832bdb5cbd0e3d7
santiDev <androidsan94@gmail.com>
2022-02-17 18:26:10 -0600

    check more override methods

--- app/src/main/java/com/santidev/helloworld/MainActivity.java
+++ app/src/main/java/com/santidev/helloworld/MainActivity.java
@@ -1,9 +1,12 @@
 package com.santidev.helloworld;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,5 +18,15 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Log.d(TAG, "onCreate: " + getString(R.string.greetings));
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        return super.onOptionsItemSelected(item);
     }
 }
