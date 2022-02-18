1c0ace92e137fd26f8c7a6cf7ba16ba5bdb676cb
santiDev <androidsan94@gmail.com>
2022-02-17 22:44:40 -0600

    finish

--- app/src/main/java/com/santidev/helloworld/MainActivity.java
+++ app/src/main/java/com/santidev/helloworld/MainActivity.java
@@ -7,17 +7,26 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "MainActivity";
-    
+    private TextView textView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         Log.d(TAG, "onCreate: " + getString(R.string.greetings));
+        Toast.makeText(this, "message", Toast.LENGTH_SHORT).show();
+
+        textView = (TextView) findViewById(R.id.textView);
+
     }
 
     @Override
@@ -29,4 +38,15 @@ public class MainActivity extends AppCompatActivity {
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
         return super.onOptionsItemSelected(item);
     }
+
+    public void topClick(View view){
+        Toast.makeText(this, "He pulsado el boton de arriba", Toast.LENGTH_SHORT).show();
+        textView.setText("He pulsado el boton de arriba");
+    }
+
+    public void bottomClick(View v){
+        Toast.makeText(this, "He pulsado el boton de abajo", Toast.LENGTH_SHORT).show();
+        textView.setText("He pulsado el boton de abajo");
+    }
+
 }
